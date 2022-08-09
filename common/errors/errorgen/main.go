ee3e8b2b27267ce080dd062e4d47ac71b5b1211c
Abirdcfly <fp544037857@gmail.com>
2022-08-09 15:58:55 +0800

    delete minor unreachable code caused by log.Fatal
    Signed-off-by: Abirdcfly <fp544037857@gmail.com>

--- common/errors/errorgen/main.go
+++ common/errors/errorgen/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-
+	
 	"v2ray.com/core/common"
 )
 
@@ -19,20 +19,19 @@ func main() {
 	if pkg == "v2ray-core" {
 		pkg = "core"
 	}
-
+	
 	moduleName, gmnErr := common.GetModuleName(pwd)
 	if gmnErr != nil {
 		fmt.Println("can not get module path", gmnErr)
 		os.Exit(1)
 	}
-
+	
 	file, err := os.OpenFile("errors.generated.go", os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatalf("Failed to generate errors.generated.go: %v", err)
-		os.Exit(1)
 	}
 	defer file.Close()
-
+	
 	fmt.Fprintln(file, "package", pkg)
 	fmt.Fprintln(file, "")
 	fmt.Fprintln(file, "import \""+moduleName+"/common/errors\"")
